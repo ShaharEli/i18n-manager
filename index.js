1489df65274b647f594314920089ea26840c213a
Shahar Eliyahu <shahar.e3@gmail.com>
2021-05-07 19:35:05 +0300

    up

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+const { execSync } = require("child_process");
 const cli = () => {
-  console.log("ya");
+  execSync("node ./server/build/index.js hi", { stdio: "inherit" });
 };
 
 module.exports = { cli };
---
+++ server/build/index.js
@@ -0,0 +1,9 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const app_1 = __importDefault(require("./app"));
+app_1.default.listen(1818, () => {
+    console.log("app listening on port 1818", process.argv);
+});
